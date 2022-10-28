a9dff0b677d5c34161a58124090875974fa83059
Hai Pho Tran <haiphotran@gmail.com>
2022-10-28 21:21:29 +0700

    update

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import './SingleCard.css'
 
-export default function SingleCard({card, handleChoice}) {
+export default function SingleCard({card, handleChoice, flipped}) {
     const handleOnclick = () => {
         handleChoice(card)
     }
     
     return (
         <div className='card'>
-            <div>
-                <img className='cardfront' src={card.src} alt='card front' />
+            <div className={flipped ? 'flipped' : ''}>
+                <img className='front' src={card.src} alt='card front' />
                 <img 
-                    className='cardback' 
+                    className='back' 
                     src='/img/cover.png' 
                     alt='card back' 
                     onClick={handleOnclick}
